14d599d8bcb64b6c7952ec5fe17011c134f57fcd
IGJY <3124056938@qq.com>
2024-09-30 14:25:46 +0800

    init project & init style

--- codeReview-front-end/src/components/index.vue
+++ codeReview-front-end/src/components/index.vue
@@ -1,11 +1,51 @@
 <script setup lang="ts">
-
+import { ref } from 'vue'
+const textarea = ref('')
 </script>
 
 <template>
-  <p>111111</p>
+  <el-container class="container">
+    <el-header>Header</el-header>
+    <el-main>
+      <el-space>
+        <div>
+          <el-row>
+            <div>
+              <el-input class="input" v-model="textarea" style="width: 240px" :rows="30" type="textarea"
+                placeholder="Please input" />
+              <el-input class="input" v-model="textarea" style="width: 240px" :rows="30" type="textarea"
+                placeholder="Please input" />
+              <el-input class="input" v-model="textarea" style="width: 240px" :rows="30" type="textarea"
+                placeholder="Please input" />
+            </div>
+          </el-row>
+          <el-row class="button">
+            <div>
+              <el-button type="primary" @click="() => { }">Submit</el-button>
+              <el-button type="primary" @click="() => { }">correct</el-button>
+            </div>
+          </el-row>
+        </div>
+      </el-space>
+    </el-main>
+  </el-container>
 </template>
 
 <style scoped>
+.container {
+  height: 100vh;
+  width: 100vw;
+}
 
+.input {
+  margin: 20px;
+}
+
+.button {
+  margin: 20px;
+  /* 居中 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
